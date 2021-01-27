db801391af9518c3b3bddf5f2f8e7b602745a6c6
Diab <59467981+TLITC-Diab@users.noreply.github.com>
2021-01-27 13:39:30 -0500

    Finished Walk Code
    Waiting for look code to combine the 2 competently

--- Dhaliwals Adventure/Assets/Scripts/PlayerMoveScript.cs
+++ Dhaliwals Adventure/Assets/Scripts/PlayerMoveScript.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour
 {
+
+    public int speed;
+    public int jumpForce;
+    public int gravity;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Debug.Log(v + h);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Debug.Log(x);
+        Debug.Log(z);
+        Vector3 moveBy = (transform.right * x + transform.forward * z);
+
+        rb.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
+
     }
 }
